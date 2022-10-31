a31d42633dc1f598d83671d498e417d2a75e3050
Shelly <shelly@adpost.com.tw>
2022-10-31 18:29:52 +0800

    feat：修改modal樣式＋新增input

--- src/components/OrderModal.vue
+++ src/components/OrderModal.vue
@@ -8,88 +8,173 @@
     aria-hidden="true"
     ref="modal"
   >
-   <!-- 請同學自行新增 v-model -->
-<div class="modal-dialog modal-xl" role="document">
-  <div class="modal-content border-0">
-    <div class="modal-header">
-      <h5 class="modal-title" id="exampleModalLabel">
-        <span>新增預約</span>
-      </h5>
-      <button type="button" class="btn-close"
-              data-bs-dismiss="modal" aria-label="Close"></button>
-    </div>
-    <div class="modal-body">
-        <div>
-          <label for="">標題</label>
-          <input type="text" v-model="tempOrder.title">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-light bg-gradient">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
-        <div>
-          <label for="">開始時間</label>
-          <input type="datetime-local" v-model="tempOrder.start" >
-        </div>
-        <div>
-          <label for="">結束時間</label>
-          <input type="datetime-local" v-model="tempOrder.end" >
-        </div>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-outline-secondary"
-              data-bs-dismiss="modal">取消
-      </button>
-      <button type="button"  v-if="isNew" class="btn btn-primary" @click="addEvent"
->新增</button>
+        <div class="modal-body">
+          <div class="mb-3 grid">
+            <label for="" class="g-col-1 col-form-label"></label>
+            <div class="g-col-11">
+              <input
+                type="text"
+                class="form-control-plaintext border-bottom"
+                v-model="tempOrder.title"
+                placeholder="新增標題"
+              />
+            </div>
+          </div>
 
-     <button type="button" v-if="!isNew" class="btn btn-primary" @click="updateEvent"
->編輯</button>
-   <button type="button" v-if="!isNew" class="btn btn-danger" @click="removeEvent"
->刪除</button>
+          <div class="mb-3 grid" v-if="tempOrder.allDay">
+            <label for="" class="g-col-1">
+              <i class="fa-regular fa-clock"></i
+            ></label>
+            <div class="g-col-11">
+              <input
+                type="date"
+                class="border-0"
+                v-model="tempOrder.start"
+                style="max-width: 45%"
+                required
+              />
+              -
+              <input
+                type="date"
+                class="border-0"
+                v-model="tempOrder.end"
+                style="max-width: 45%"
+                required
+              />
+            </div>
+          </div>
 
+          <div class="mb-3 grid" v-else>
+            <label for="" class="g-col-1">
+              <i class="fa-regular fa-clock"></i
+            ></label>
+            <div class="g-col-11">
+              <input
+                type="datetime-local"
+                class="border-0"
+                v-model="tempOrder.start"
+                style="max-width: 45%"
+              />
+              -
+              <input
+                type="datetime-local"
+                class="border-0"
+                v-model="tempOrder.end"
+                style="max-width: 45%"
+              />
+            </div>
+          </div>
+
+          <div class="mb-3 grid">
+            <label for="" class="g-col-1"></label>
+            <div class="form-check g-col-11">
+              <input
+                type="checkbox"
+                name=""
+                id="allDayCheck"
+                class="form-check-input"
+                v-model="tempOrder.allDay"
+              />
+              <label for="allDayCheck" class="form-check-label">全天</label>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-outline-secondary"
+            data-bs-dismiss="modal"
+          >
+            取消
+          </button>
+          <button
+            type="button"
+            v-if="isNew"
+            class="btn btn-primary"
+            @click="addEvent"
+          >
+            新增
+          </button>
+
+          <button
+            type="button"
+            v-if="!isNew"
+            class="btn btn-primary"
+            @click="updateEvent"
+          >
+            更新
+          </button>
+          <button
+            type="button"
+            v-if="!isNew"
+            class="btn btn-danger"
+            @click="removeEvent"
+          >
+            刪除
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   </div>
 </template>
 
 <script>
-import modalMixin from '@/mixins/modalMixin'
+import modalMixin from "@/mixins/modalMixin";
 export default {
   props: {
     order: {
-      type: Object
+      type: Object,
     },
     isNew: {
-      type: Boolean
-    }
+      type: Boolean,
+    },
   },
-  data () {
+  data() {
     return {
       modal: {},
-      tempOrder: {}
-    }
+      tempOrder: {},
+    };
   },
   methods: {
-    addEvent () {
-      this.$emit('add-event', {
+    addEvent() {
+      this.$emit("add-event", {
         id: Math.floor(Date.now()),
         title: this.tempOrder.title,
         start: this.tempOrder.start,
-        end: this.tempOrder.end
-      })
+        end: this.tempOrder.end,
+        allDay: this.tempOrder.allDay,
+      });
     },
-    updateEvent () {
-      this.$emit('update-event', this.tempOrder)
+    updateEvent() {
+      this.$emit("update-event", this.tempOrder);
     },
-    removeEvent () {
-      this.$emit('remove-event', this.tempOrder.event)
-    }
+    removeEvent() {
+      this.$emit("remove-event", this.tempOrder.event);
+    },
   },
   watch: {
-    order () {
-      this.tempOrder = this.order
-      console.log('tempOrder', this.tempOrder)
-    }
+    order() {
+      this.tempOrder = this.order;
+      console.log("tempOrder", this.tempOrder);
+    },
   },
-  mixins: [modalMixin]
-}
-
+  mixins: [modalMixin],
+};
 </script>
+<style lang="scss">
+input:focus-visible {
+  background-color: rgb(248, 249, 250);
+  border-bottom: 1px solid#0d6efd !important;
+  outline: none;
+}
+</style>
