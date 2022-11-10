ab153bd3bbabe4e7e66f2de2f8f625aabf9ebddf
Shelly <shelly@adpost.com.tw>
2022-11-10 14:37:46 +0800

    chore:部署設定

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# 發生錯誤時執行終止指令
+set -e
+
+# 打包編譯
+npm run build
+
+# 移動到打包資料夾下
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+git push -f https://github.com/hahaalin/calendar.git master:gh-pages
+
+cd -
